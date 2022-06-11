bfbcccd3383a53a17d62d8274456492d53349a45
Anita Deszczynska <anitadeszczynska@gmail.com>
2022-06-11 21:46:05 +0200

    Create a timer project in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import FormattedTime from './components/FormattedTime/FormattedTime';
+import Button from './components/Button/Button';
+import Container from './components/Container/Container';
 
-function App() {
+const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const start = () => {
+    setTimer(setInterval(() => {
+      setTime(preValue => preValue + 0.01);
+    }, 10))
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+    setTimer();
+  }
+
+  const reset = () => {
+    setTime(0);
+    setTimer(0);
+  }
+
+  useEffect(() => {
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <FormattedTime>{time}</FormattedTime>
+      <Button value={start}>start</Button>
+      <Button value={stop}>stop</Button>
+      <Button value={reset}>reset</Button>
+    </Container>
   );
-}
+};
 
 export default App;
